16289f0a1dcbc067b9fd089285d4ef951ef1d978
redaphid <iam@aaronherres/com>
2023-11-29 23:20:28 -0700

    seems to work

--- src/AudioVisualizer.tsx
+++ src/AudioVisualizer.tsx
@@ -1,72 +1,133 @@
 import React, { useRef, useEffect, useContext } from 'react';
-import { AudioProviderContext } from './AudioProvider'; // Adjust the import path as needed
+import { AudioProviderContext } from './AudioProvider';
 
-// Vertex Shader
-const vertexShaderSource = `
+export const AudioVisualizer = () => {
+  const canvasRef = useRef(null);
+  const shaderProgramRef = useRef(null);
+  const glRef = useRef(null); // To store the WebGL context
+  const audioContext = useContext(AudioProviderContext);
+
+  // Vertex Shader
+  const vertexShaderSource = `
     attribute vec4 aVertexPosition;
     void main() {
         gl_Position = aVertexPosition;
     }
-`;
+  `;
 
-// Fragment Shader
-const fragmentShaderSource = `
+  // Fragment Shader
+  const fragmentShaderSource = `
     precision mediump float;
     uniform vec4 uColor;
     void main() {
         gl_FragColor = uColor;
     }
-`;
-
-
-export const AudioVisualizer = () => {
-  const canvasRef = useRef(null);
-  const audioContext = useContext(AudioProviderContext);
-  const glRef = useRef(null); // To store the WebGL context
-
-  // Function to update color based on RMS
-  const updateColor = (gl, rms) => {
-    // Normalize RMS value for color intensity (assuming rms is between 0 and 1)
-    const intensity = rms ? Math.min(1, Math.max(0, rms)) : 0;
-    const color = [intensity, 0.0, 0.0, 1.0]; // Red color with varying intensity
-
-    gl.clearColor(...color);
-    gl.clear(gl.COLOR_BUFFER_BIT);
-  };
+  `;
 
   useEffect(() => {
-    if (!audioContext || !canvasRef.current) {
+    if (!canvasRef.current || glRef.current) {
       return;
     }
 
-    const canvas = canvasRef.current;
-    const gl = canvas.getContext('webgl');
+    const gl = canvasRef.current.getContext('webgl');
     glRef.current = gl;
 
     if (!gl) {
       console.error('Unable to initialize WebGL. Your browser may not support it.');
       return;
     }
+   // Function to create a shader
+   const loadShader = (type, source) => {
+    const shader = gl.createShader(type);
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+      console.error('An error occurred compiling the shaders: ' + gl.getShaderInfoLog(shader));
+      gl.deleteShader(shader);
+      return null;
+    }
+    return shader;
+  };
 
-    // Setup WebGL environment
-    gl.clearColor(0.0, 0.0, 0.0, 1.0); // Default clear color (black)
-    gl.clear(gl.COLOR_BUFFER_BIT);
-
-    // Rendering loop
-    const render = () => {
-      if (audioContext.audioFeatures?.rms !== undefined) {
-        updateColor(gl, audioContext.audioFeatures.rms);
+  // Initialize shaders
+  const vertexShader = loadShader(gl.VERTEX_SHADER, vertexShaderSource);
+  const fragmentShader = loadShader(gl.FRAGMENT_SHADER, fragmentShaderSource);
+     const shaderProgram = gl.createProgram();
+     shaderProgramRef.current = shaderProgram;
+     gl.attachShader(shaderProgram, vertexShader);
+     gl.attachShader(shaderProgram, fragmentShader);
+     gl.linkProgram(shaderProgram);
+       // Check if shader program linked successfully
+    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+        console.error('Unable to initialize the shader program: ' + gl.getProgramInfoLog(shaderProgram));
+        return;
       }
 
-      requestAnimationFrame(render);
+       // Use the shader program
+    gl.useProgram(shaderProgram);
+
+    // Set up the rectangle coordinates (two triangles)
+    const vertices = [
+        -1.0,  1.0,
+        -1.0, -1.0,
+         1.0,  1.0,
+         1.0, -1.0,
+    ];
+    const vertexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+
+    // Get the position attribute location from the shader program
+    const positionAttribLocation = gl.getAttribLocation(shaderProgram, 'aVertexPosition');
+    gl.vertexAttribPointer(positionAttribLocation, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(positionAttribLocation);
+
+
+
+    return () => {
+      // Cleanup WebGL resources
+      if (shaderProgramRef.current) {
+        gl.deleteProgram(shaderProgramRef.current);
+        shaderProgramRef.current = null;
+      }
+      gl.deleteShader(fragmentShader);
+      gl.deleteShader(vertexShader);
+      gl.deleteBuffer(vertexBuffer);
+    };
+  }, []); // Empty dependency array to run only once
+
+  useEffect(() => {
+    if (!audioContext || !shaderProgramRef.current) {
+      return;
+    }
+
+    const gl = glRef.current;
+    const shaderProgram = shaderProgramRef.current;
+    const colorUniformLocation = gl.getUniformLocation(shaderProgram, 'uColor');
+      // Rendering loop
+      const render = () => {
+        if (!shaderProgramRef.current) return;
+
+        // Clear the canvas
+        gl.clear(gl.COLOR_BUFFER_BIT);
+
+        // Set the color based on the RMS value
+        const rms = audioContext.audioFeatures?.rms || 0;
+        const color = [rms, 0.0, 0.0, 1.0]; // Red color with varying intensity
+        gl.uniform4fv(colorUniformLocation, color);
+
+        // Draw the rectangle
+        gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+
+        requestAnimationFrame(render);
     };
     requestAnimationFrame(render);
 
-    // Cleanup
     return () => {
-      // WebGL cleanup (if any required)
+      // Cleanup for rendering loop
     };
-  }, [audioContext]); // Effect dependency
+  }, [audioContext]); // Dependency on audioContext for updates
+
 
   return (
     <div>
