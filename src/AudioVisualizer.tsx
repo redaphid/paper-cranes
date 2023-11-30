19f0c5b67a26fcc010960b1ebf546cd0d784d7d3
redaphid <iam@aaronherres/com>
2023-11-29 23:44:28 -0700

    it works!

--- src/AudioVisualizer.tsx
+++ src/AudioVisualizer.tsx
@@ -8,87 +8,91 @@ const vertexShaderSource = `
     }
 `;
 
-export const AudioVisualizer = ({ fragmentShaderSource }) => {
-  const canvasRef = useRef(null);
-  const shaderProgramRef = useRef(null);
-  const audioContext = useContext(AudioProviderContext);
+const setupShader = (gl, type, source) => {
+    const shader = gl.createShader(type);
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
 
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    const gl = canvas.getContext('webgl');
-    if (!gl) {
-      console.error('Unable to initialize WebGL. Your browser may not support it.');
-      return;
-    }
-
-    // Function to create a shader
-    const loadShader = (type, source) => {
-      const shader = gl.createShader(type);
-      gl.shaderSource(shader, source);
-      gl.compileShader(shader);
-      if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
         console.error('An error occurred compiling the shaders: ' + gl.getShaderInfoLog(shader));
         gl.deleteShader(shader);
         return null;
-      }
-      return shader;
-    };
+    }
 
-    // Initialize shaders
-    const vertexShader = loadShader(gl.VERTEX_SHADER, vertexShaderSource);
-    const fragmentShader = loadShader(gl.FRAGMENT_SHADER, fragmentShaderSource);
+    return shader;
+};
 
-    // Create the shader program
+const createShaderProgram = (gl, vertexShaderSource, fragmentShaderSource) => {
+    const vertexShader = setupShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+    const fragmentShader = setupShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
+
     const shaderProgram = gl.createProgram();
-    shaderProgramRef.current = shaderProgram;
     gl.attachShader(shaderProgram, vertexShader);
     gl.attachShader(shaderProgram, fragmentShader);
     gl.linkProgram(shaderProgram);
 
     if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-      console.error('Unable to initialize the shader program: ' + gl.getProgramInfoLog(shaderProgram));
-      return;
+        console.error('Unable to initialize the shader program: ' + gl.getProgramInfoLog(shaderProgram));
+        return null;
     }
 
-    gl.useProgram(shaderProgram);
+    return shaderProgram;
+};
 
-    // Set up the rectangle coordinates (two triangles)
-    const vertices = [-1.0, 1.0, -1.0, -1.0, 1.0, 1.0, 1.0, -1.0];
-    const vertexBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+export const AudioVisualizer = ({ fragmentShaderSource }) => {
+    const canvasRef = useRef(null);
+    const shaderProgramRef = useRef(null);
+    const colorUniformLocationRef = useRef(null);
+    const audioContext = useContext(AudioProviderContext);
 
-    const positionAttribLocation = gl.getAttribLocation(shaderProgram, 'aVertexPosition');
-    gl.vertexAttribPointer(positionAttribLocation, 2, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(positionAttribLocation);
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        const gl = canvas.getContext('webgl');
+        if (!gl) {
+            console.error('Unable to initialize WebGL. Your browser may not support it.');
+            return;
+        }
 
-    const colorUniformLocation = gl.getUniformLocation(shaderProgram, 'uColor');
+        const shaderProgram = createShaderProgram(gl, vertexShaderSource, fragmentShaderSource);
+        if (!shaderProgram) return;
 
-    const render = () => {
-      if (!shaderProgramRef.current) return;
+        shaderProgramRef.current = shaderProgram;
+        gl.useProgram(shaderProgram);
+        colorUniformLocationRef.current = gl.getUniformLocation(shaderProgram, 'uColor');
 
-      gl.clear(gl.COLOR_BUFFER_BIT);
+        const vertices = [-1.0, 1.0, -1.0, -1.0, 1.0, 1.0, 1.0, -1.0];
+        const vertexBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 
-      const rms = audioContext.audioFeatures?.rms || 0;
-      const color = [rms, 0.0, 0.0, 1.0]; // Red color with varying intensity
-      gl.uniform4fv(colorUniformLocation, color);
+        const positionAttribLocation = gl.getAttribLocation(shaderProgram, 'aVertexPosition');
+        gl.vertexAttribPointer(positionAttribLocation, 2, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(positionAttribLocation);
 
-      gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
-      requestAnimationFrame(render);
-    };
-    requestAnimationFrame(render);
+        const render = () => {
+            if (!shaderProgramRef.current) return;
 
-    return () => {
-      if (shaderProgramRef.current) {
-        gl.deleteProgram(shaderProgramRef.current);
-      }
-      gl.deleteShader(fragmentShader);
-      gl.deleteShader(vertexShader);
-      gl.deleteBuffer(vertexBuffer);
-    };
-  }, [audioContext, fragmentShaderSource]);
+            gl.useProgram(shaderProgramRef.current);
+            gl.clear(gl.COLOR_BUFFER_BIT);
 
-  return <canvas ref={canvasRef} width="640" height="480"></canvas>;
+            const rms = audioContext.audioFeatures?.rms || 0;
+            const color = [rms, 0.0, 0.0, 1.0];
+            gl.uniform4fv(colorUniformLocationRef.current, color);
+
+            gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+            requestAnimationFrame(render);
+        };
+        requestAnimationFrame(render);
+
+        return () => {
+            if (shaderProgramRef.current) {
+                gl.deleteProgram(shaderProgramRef.current);
+            }
+            gl.deleteBuffer(vertexBuffer);
+        };
+    }, [fragmentShaderSource, audioContext]);
+
+    return <canvas ref={canvasRef} width="640" height="480"></canvas>;
 };
 
 export default AudioVisualizer;
--- src/app.tsx
+++ src/app.tsx
@@ -1,18 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { AudioProvider } from './AudioProvider'; // Adjust the import path as needed
-import {AudioVisualizer} from './AudioVisualizer'; // Adjust the import path as needed
+import { AudioVisualizer } from './AudioVisualizer'; // Adjust the import path as needed
 
 export const App = () => {
-    const fragmentShader = `
-      precision mediump float;
-      uniform vec4 uColor;
-      void main() {
-          gl_FragColor = uColor;
-      }
-    `;
-    return (
-    <AudioProvider>
-         <AudioVisualizer fragmentShaderSource={fragmentShader} />
-    </AudioProvider>
-    );
+  const initialShaderSource = `
+    precision mediump float;
+    uniform vec4 uColor;
+    void main() {
+        gl_FragColor = uColor;
+    }
+  `;
+
+  const [shaderSource, setShaderSource] = useState(initialShaderSource);
+  const [currentShaderSource, setCurrentShaderSource] = useState(initialShaderSource);
+
+  const handleShaderSourceChange = (event) => {
+    setShaderSource(event.target.value);
   };
+
+  const handleUpdateClick = () => {
+    setCurrentShaderSource(shaderSource);
+  };
+
+  return (
+    <AudioProvider>
+      <textarea
+        value={shaderSource}
+        onChange={handleShaderSourceChange}
+        rows="10"
+        cols="50"
+      />
+      <button onClick={handleUpdateClick}>Update</button>
+      <AudioVisualizer fragmentShaderSource={currentShaderSource} />
+    </AudioProvider>
+  );
+};
